4d82271f4430b7f4ffb98eb0cebbd9389cbef5e1
fahfiewww <farz.fj@gmail.com>
2020-08-03 15:41:40 +0700

    tc1

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,30 +3,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
+
     @Test
-    public void sayHi_should_return_Hello_World(){
+    public void sayHi_should_return_Hello_World() {
         HelloWorld h = new HelloWorld();
-        String result = h.sayHi("Hello World");
+        String result = h.sayHi();
         assertEquals("Hello World", result);
-
     }
+
     @Test
-    public void sayHi_with_simkiat_should_return_Hello_somkiat(){
+    public void sayHi_with_somkiat_should_return_Hello_Somkiat() {
         HelloWorld h = new HelloWorld();
-        String result = h.sayHi("Somkiat");
-        assertEquals("Hello Somkiat", result);
-
+        String result = h.sayHi("somkiaT");
+        assertEquals("Hello SomkiaT", result);
     }
+
     @Test
-    public void sayHi_with_pui_should_return_Hello_pui(){
+    public void sayHi_with_pui_should_return_Hello_Pui() {
         HelloWorld h = new HelloWorld();
         String result = h.sayHi("pui");
         assertEquals("Hello Pui", result);
-
-    }
-
-    public void yyyy(){
-
     }
 
 }
